3d19a56074af098fc3048d50bc21ffa4d80daae1
codett <casey.dettlaff@gmail.com>
2025-05-14 11:10:50 -0700

    Wrote improved function for filtering IDF and EPW filepaths for automated generation of multiple buildings.

--- Data_Generation/EP_DataGenerator_Script_v2_20250512.py
+++ Data_Generation/EP_DataGenerator_Script_v2_20250512.py
@@ -14,6 +14,7 @@ THIS_SCRIPT_DIR = os.path.dirname(__file__)
 SPECIAL_IDF_FILEPATH = os.path.join('Special.idf')
 TEMPORARY_FOLDERPATH = os.path.join(THIS_SCRIPT_DIR, 'Temporary Folder')
 
+default_data_folderpath = os.path.join(THIS_SCRIPT_DIR, '..', 'Data')
 default_results_folderpath = os.path.join(THIS_SCRIPT_DIR, '..', 'Results')
 default_simulation_settings = {
     "name": "new_simulation",
@@ -62,8 +63,9 @@ default_simulation_variable_names = ['Schedule Value',
                                   'System Node Mass Flow Rate']
 
 # Test
-TEST_IDF_FILEPATH = r"D:\Building_Modeling_Code\Data\Commercial_Prototypes\ASHRAE\90_1_2013\ASHRAE901_OfficeSmall_STD2013_Seattle.idf"
+TEST_IDF_FILEPATH = r"C:\Users\codett\Downloads\ASHRAE901_OfficeLarge_STD2013_Seattle_debugging.idf"
 TEST_EPW_FILEPATH = r"D:\Building_Modeling_Code\Data\TMY3_WeatherFiles_Commercial\USA_WA_Seattle-Tacoma.Intl.AP.727930_TMY3.epw"
+TEST_DATA_FOLDERPATH = r"D:\Building_Modeling_Code\Data"
 
 def generate_variables(idf_filepath, epw_filepath):
 
@@ -278,4 +280,91 @@ def simulate_variables(idf_filepath, epw_filepath, variable_names, simulation_se
 
         pickle.dump(eio_dict, open(eio_pickle_filepath, 'wb'))
 
-simulate_variables(TEST_IDF_FILEPATH, TEST_EPW_FILEPATH, default_simulation_variable_names)
+# simulate_variables(TEST_IDF_FILEPATH, TEST_EPW_FILEPATH, default_simulation_variable_names)
+
+def get_location_climate_zone(location=None, climate_zone=None):
+
+    climate_zones = {
+        "Miami": "1A",
+        "Tampa": "2A",
+        "Tucson": "2B",
+        "Atlanta": "3A",
+        "ElPaso": "3B",
+        "SanDiego": "3C",
+        "NewYork": "4A",
+        "Albuquerque": "4B",
+        "Seattle": "4C",
+        "Buffalo": "5A",
+        "Denver": "5B",
+        "PortAngeles": "5C",
+        "Rochester": "6A",
+        "GreatFalls": "6B",
+        "InternationalFalls": "7",
+        "Fairbanks": "8"
+    }
+
+    if location: return climate_zones.get(location, "Climate Zone not found.")
+    if climate_zone:
+        reversed_climate_zones = {v: k for k, v in climate_zones.items()}
+        return reversed_climate_zones.get(climate_zone, "Location not found.")
+
+def get_idf_weather_filepaths(building_type, idf_filter_list, idf_weather_df=None, data_folderpath=default_data_folderpath):
+
+    # Initialize file lists
+    all_idf_filepath_list = []
+    all_epw_filepath_list = []
+
+    # If no `idf_weather_df` is provided, collect all .idf and .epw files from the data folder
+    if idf_weather_df is None:
+        for root, _, files in os.walk(data_folderpath):
+            for file in files:
+                if file.endswith(".idf") and building_type in root:
+                    all_idf_filepath_list.append(os.path.join(root, file))
+                elif file.endswith(".epw"):
+                    all_epw_filepath_list.append(os.path.join(root, file))
+        idf_files = all_idf_filepath_list
+        epw_files = all_epw_filepath_list
+    else:
+        # Get idf and epw file lists from `idf_weather_df`
+        idf_files = idf_weather_df['filtered_idf_filepath'].tolist()
+        epw_files = idf_weather_df['filtered_epw_filepath'].dropna().tolist()
+
+    # Filter IDF files based on `idf_filter_list` (2D list)
+    filtered_idf_files = []
+    for filters in idf_filter_list:  # Each inner list represents a set of filters
+        for idf in idf_files:
+            if all(f in os.path.basename(idf) for f in filters):  # All filters must match
+                filtered_idf_files.append(idf)
+
+    # Create a dataframe to store mapping of filtered IDF and EPW filepaths
+    data = []
+    for idf in filtered_idf_files:
+        idf_filename = os.path.basename(idf)
+        if building_type == 'Commercial':
+            location = idf_filename.split('_')[3].replace('.idf', '')
+        elif building_type == 'Residential':
+            climate_zone = idf_filename.split('+')[2]
+            location = get_location_climate_zone(climate_zone=climate_zone)
+        elif building_type == 'Manufactured':
+            location = idf_filename.split('_')[1]
+        else:
+            location = None
+
+        # Find the corresponding EPW file for the location
+        corresponding_epw = None
+        for epw in epw_files:
+            epw_filename = os.path.basename(epw).replace('.', '')
+            if building_type in epw and location in epw_filename:
+                corresponding_epw = epw
+                break
+
+        data.append({'filtered_idf_filepath': idf, 'filtered_epw_filepath': corresponding_epw})
+
+    # Return as a dataframe
+    return pd.DataFrame(data)
+
+idf_filter_list = [['Seattle']]
+df = get_idf_weather_filepaths('Commercial', idf_filter_list, data_folderpath=TEST_DATA_FOLDERPATH)
+idf_filter_list = [['Hospital'], ['ApartmentHighRise'], ['ApartmentMidRise']]
+df = get_idf_weather_filepaths('Commercial', idf_filter_list, idf_weather_df=df)
+print(df)
